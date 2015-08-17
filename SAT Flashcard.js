66de028c2cf08b9adf381056b93d1673b66fff2f
Jenniber Franco <jfranco7156@bths.edu>
2015-08-17 14:21:18 -0400

    First commit

---
+++ SAT Flashcard.js
@@ -0,0 +1,559 @@
+//Basic(less than 6 letters)
+//pos= part of speech
+var evade ={
+	'pos': 'verb',
+	'def': 'to dodge/avoid',
+	'translation': {
+		'spanish': 'evadir',
+        'english':'evade'
+	}
+};
+
+var feign ={
+	'pos': 'verb',
+	'def': 'pretend',
+	'translation': {
+		'spanish': 'fingir',
+        'english':'feign'
+	}
+}; 
+
+var grim ={
+	'pos': 'adjective',
+	'def': 'serious',
+	'translation':{
+		'spanish':'serio(a)',
+        'english':'grim'
+	}
+};
+
+var exalt ={
+	'pos': 'verb',
+	'def': 'to praise',
+	'translation':{
+		'spanish': 'exaltar',
+        'english':'exalt'
+	}
+};
+
+var fetid ={
+	'pos': 'adjective',
+	'def': 'malodorous',
+	'translation':{
+		'spanish': 'fetido(a)',
+        'english':'fetid'
+	}
+};
+
+var filch ={
+	'pos': 'verb',
+	'def': 'to steal',
+	'translation':{
+		'spanish': 'robar',
+        'english':'filch'
+	}
+};
+
+var flair ={
+	'pos': 'noun',
+	'def': 'talent',
+	'translation':{
+		'spanish': 'talento',
+        'english':'flair'
+	}
+};
+
+var flout ={
+	'pos': 'verb',
+	'def': 'comtempt',
+	'translation':{
+		'spanish': 'desobeder(reglas o autoridad)',
+        'english':'flout'
+	}
+};
+
+var foil ={
+	'pos': 'verb',
+	'def': 'defeat',
+	'translation':{
+		'spanish': 'desbaratar los planes de',
+        'english': 'foil'
+	}
+};
+
+var forgo ={
+	'pos': 'verb',
+	'def': 'give up',
+	'translation':{
+		'spanish': 'abstenerse',
+        'english':'forgo'
+	}
+};
+
+var glut ={
+	'pos': 'noun',
+	'def': 'fill to excess',
+	'translation':{
+		'spanish': 'exceso(m)/ superabundancia(f)',
+        'english':'glut'
+	}
+};
+
+var guile ={
+	'pos': 'noun',
+	'def': 'deceit',
+	'translation':{
+		'spanish': 'astucia',
+        'english': 'guile'
+	}
+};
+
+var guru ={
+	'pos': 'noun',
+	'def': 'expert',
+	'translation':{
+		'spanish': 'gurú',
+		'english': 'guru',
+	}
+}
+
+var hoard ={
+	'def': '(v.) - save',
+	'translation':{
+		'spanish': 'acumular',
+		'english': 'hoard'
+	}
+}
+
+var impel ={
+	'pos': 'verb',
+	'def': 'force onward',
+	'translation':{	
+		'spanish': 'impulsar',
+		'english': 'impel'
+	}
+}
+
+var inane ={
+	'pos': 'adjective',
+	'def': 'silly',
+	'translation':{
+		'spanish': 'sonso',
+		'english': 'inane'
+	}
+}
+
+var inept ={
+	'pos': 'adjective',
+	'def': 'lacking skill',
+	'translation':{
+		'spanish': 'inepto',
+		'english': 'inept'
+	}
+}
+
+var inert ={
+	'pos': 'adjective',
+	'def': 'inactive',
+	'translation':{
+		'spanish': 'inmóvil',
+		'english': 'inert'
+	}
+}
+
+var iota ={
+	'pos': 'noun',
+	'def': 'small quality',
+	'translation':{
+		'spanish': 'pizca',
+		'english': 'iota'
+	}
+}
+
+var jest ={
+	'pos':'noun',
+	'def': 'joke',
+	'translation':{
+		'spanish': 'broma',
+		'english': 'jest'
+	}
+}
+
+var jibe ={
+	'pos': 'verb',
+	'def': 'mock',
+	'translation':{
+		'spanish': 'burlarse',
+		'english': 'jibe'
+	}
+}
+
+var lax ={,
+	'pos': 'adjective',
+	'def': 'careless',
+	'translation':{
+		'spanish': 'relajado(a)',
+		'english': 'lax'
+	}
+}
+
+var leery ={
+	'pos': 'adjective',
+	'def': 'cautious',
+	'translation':{
+		'spanish': 'cauteloso',
+		'english': 'leery'
+	}
+} 
+
+var limp ={
+	'pos': 'adjective',
+	'def': 'no energy',
+	'translation':{
+		'spanish': 'flojo(a)',
+		'english': 'limp'
+	}
+}
+
+var lull ={
+	'pos': 'verb',
+	'def': 'soothe',
+	'translation':{
+		'spanish': 'calmar',
+		'english': 'lull'
+	}
+}
+
+var lush ={
+	'pos': 'adjective',
+	'def': 'abundant',
+	'translation':{
+		'spanish': 'lujoso(a)',
+		'english': 'lush'
+	}
+}
+
+var meek ={
+	'pos': 'adjective',
+	'def': 'obedient',
+	'translation':{
+		'spanish': 'dócil',
+		'english': 'meek'	
+	}
+};
+
+//Medium(6-9 letters)
+var esteem = {
+	'pos': 'adjective',
+	'def': 'respect',
+	'translation':{
+		'spanish': 'apreciar',
+		'english': 'esteem'
+	}
+};
+
+var exacting = {
+	'pos': 'adjective',
+	'def': 'extremely demanding',
+	'translation':{
+		'spanish': 'exigente',
+		'english': 'exacting'
+	}
+};
+
+var exegesis = {
+	'pos': 'noun',
+	'def': 'explanation',
+	'translation':{
+		'spanish': 'explicación',
+		'english': 'exegesis'
+	}
+};
+
+var faction = {
+	'pos': 'noun',
+	'def': '(n.)- party',
+	'translation':{
+		'spanish': 'faccion',
+		'english': 'faction'
+	}
+};
+
+var fathom = {
+	'pos': 'verb',	
+	'def': 'comprehend',
+	'translation':{
+		'spanish': 'comprender',
+		'english': 'fathom',
+	}
+};
+
+var fervent = {
+	'pos': 'adjective',
+	'def': 'ardent',
+	'translation':{
+		'spanish': 'enfático',
+		'english': 'fervent'
+
+	}
+};
+
+var forswear = {
+	'pos': 'adjective',
+	'def': 'abandon',
+	'translation':{
+		'spanish': 'abandonar',
+		'english': 'forswear'
+	}
+};
+
+var fraught = {
+	'pos': 'adjective',
+	'def': 'filled',
+	'translation':{
+		'spanish': 'lleno',
+		'english': 'fraught'
+	}
+};
+
+var fulminate = {
+	'pos': 'verb',
+	'def': 'criticized strongly',
+	'translation':{
+		'spanish': 'fulminante',
+		'english': 'fulminate'
+	}
+};
+
+var gratify = {
+	'pos': 'verb',
+	'def': 'please',
+	'translation':{
+		'spanish': 'gratificar',
+		'english': 'gratify'
+	}
+};
+
+var grievious = {
+	'pos': 'adjective'
+	'def': 'great pain',
+	'translation':{
+		'spanish':  'doloroso'
+		'english': 'grievious'
+	}
+};
+
+var husband = {
+	'pos': 'verb',
+	'def': ' save',
+	'translation':{
+		'spanish': 'administrar/ ahorrar',
+		'english': 'husband'
+	}
+
+};
+
+var immense = {
+	'pos': 'adjective',
+	'def': 'enormous',
+	'translation':{
+		'spanish': 'inmenso',
+		'english': 'immense'
+	}
+};
+
+var impassive = {
+	'pos': 'adjective',
+	'def': 'fair',
+	'translation':{
+		'spanish': 'imperturbable',
+		'english': 'impassive'
+	}
+};
+
+var impasse = {
+	'pos': 'noun',
+	'def': 'predicament',
+	'translation':{
+		'spanish': 'predicamento',
+		'english': 'impasse'
+	}
+};
+
+var ingrained = {
+	'pos': 'adjective',
+	'def': 'firmly rooted',
+	'translation':{
+		'spanish': 'arraigado(a)',
+		'english': 'ingrained'
+	}
+};
+
+var innocuous = {
+	'pos': 'adjective',
+	'def': 'harmless',
+	'translation':{
+		'spanish': 'inofensivo',
+		'english': 'innocuous'
+	}
+};
+
+var inundate = {
+	'pos': 'verb',
+	'def': 'flood',
+	'translation':{
+		'spanish': 'inundar',
+		'english': 'inundate'
+	}
+};
+
+var irascible= {
+	'pos': 'adjective',
+	'def': 'irritable',
+	'translation':{
+		'spanish': 'irritable',
+		'english': 'irascible'
+	}
+};
+
+var jaundiced= {
+	'pos': 'adjective',
+	'def': 'prejudiced',
+	'translation':{
+		'spanish': 'resentido(a)',
+		'english': 'jaundiced'
+	}
+};
+
+var layman= {
+	'pos': 'noun',
+	'def': 'not professional',
+	'translation':{
+		'spanish': 'profano(a)',
+		'english': 'layman'
+	}
+};
+
+var litigious= {
+	'pos': 'adjective',
+	'def': 'ready to take disputes',
+	'translation':{
+		'spanish': 'litigante',
+		'english': 'litigious'
+	}
+};
+
+var magnify= {
+	'pos': 'verb',
+	'def': 'to make something bigger',
+	'translation':{
+		'spanish': 'aumentar',
+		'english': 'magnify'
+	}
+};
+
+
+var manifest= {
+	'pos': 'adjective',
+	'def': 'evident',
+	'translation':{
+		'spanish': 'manifestar',
+		'english': 'manifest'
+	}
+};
+
+
+var marshal= {
+	'pos': 'verb',
+	'def': 'to organize',
+	'translation':{
+		'spanish': 'dirigir',
+		'english': 'marshal'
+	}
+};
+
+
+var menace= {
+	'pos': 'verb',
+	'def': 'threat',
+	'translation':{
+		'spanish': 'amenaza',
+		'english': 'menace'
+	}
+};
+
+var pristine= {
+	'pos': 'adjective',
+	'def': 'untouched',
+	'translation':{
+		'spanish': 'inmaculado(a)',
+		'english': 'pristine'
+	}
+};
+
+
+
+//Hard Level
+
+var evanescent= {
+	'pos': 'adjective',
+	'def': 'fleeting',
+	'translation':{
+		'spanish': 'fugaz',
+		'english': 'evanescent'
+	}
+};
+
+var exceptional= {
+	'pos': 'adjective',
+	'def': 'good',
+	'translation':{
+		'spanish': 'extraordinario',
+		'english': 'exceptional'
+	}
+};
+
+var excruciating= {
+	'pos': 'adjective',
+	'def': 'very intense',
+	'translation':{
+		'spanish': 'insorportable'
+		'english': 'excruciating'
+	}
+};
+
+var fastidious= {
+	'pos': 'adjective',
+	'def': 'hard to please',
+	'translation':{
+		'spanish': 'meticuloso',
+		'english': 'fastidious'
+	}
+};
+
+var felicitous= {
+	'pos': 'adjective',
+	'def': 'well chosen',
+	'translation':{
+		'spanish': 'acertado(a)',
+		'english': 'felicitous'
+	}
+};
+
+var formidable= {
+	'pos': 'adjective',
+	'def': 'menacing',
+	'translation':{
+		'spanish': 'amenazante',
+		'english': 'formidable'
+	}
+};
+
+var fraudulent= {
+	'pos': 'adjective',
+	'def': 'cheating',
+	'translation':{
+		'spanish': 'fraudulento(a)',
+		'english': 'fraudulent'
+	}
+};
